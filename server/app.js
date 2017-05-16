9c41f54802655e81590d7824a0cd3683bdf643d3
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-05-16 14:27:36 +0530

    Updated EJS template

--- server/app.js
+++ server/app.js
@@ -5,11 +5,16 @@ const app = express();
 
 // Serve static assets
 //app.use(express.static(path.resolve(__dirname, '..', 'public')));
-app.use(express.static(path.resolve('public')));
+app.use(express.static(path.resolve('public')));  // path eill be resolved from root
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, "views"));
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
+  //res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
+  res.render('layout/blank', {title: "React with Express", "body": "Hello from EJS"});
 });
 
 module.exports = app;
