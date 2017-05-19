1b19f6fca1ccf260f51e4682b077d3e2d3241862
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-05-19 10:18:16 +0530

    Added redux store in Home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,36 +1,21 @@
 import React, { Component } from 'react';
 import {NavLink} from 'react-router-dom'
 
+import store from '../reducers/reducers';
+
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {featured: [
-      {
-        id: 1,
-        slug: "course-1",
-        title: "Course 1",
-        desc: "A great course",
-        cover_image: "http://lorempixel.com/400/200/nature/"
-      },
-      {
-        id: 2,
-        slug: "course-2",
-        title: "Course 2",
-        desc: "A great course 2",
-        cover_image: "http://lorempixel.com/400/200/nature/"
-      },
-       {
-         id: 3,
-         slug: "course-3",
-        title: "Course 3",
-        desc: "A great course 3",
-        cover_image: "http://lorempixel.com/400/200/nature/"
-      }
-    ]};
+    store.dispatch({
+      type: "FEATURED_COURSE"
+    });
 
+    this.featured = store.getState();
+
+    console.log("FEATURED: ", this.featured);
   }
   render() {
-    var featured = this.state.featured.map(function (course) {
+    var featured = this.featured.home.map(function (course) {
         return( 
         <div className="col-md-4" key={course.id}>
            <h2><NavLink to={`/${course.slug}`}>{course.title}</NavLink></h2>
---
+++ app/reducers/reducers.js
@@ -0,0 +1,45 @@
+import {createStore, combineReducers, compose} from 'redux'
+
+var featuredState = [
+    {
+    id: 1,
+    slug: "course-1",
+    title: "Course 1",
+    desc: "A great course",
+    cover_image: "http://lorempixel.com/400/200/nature/"
+    },
+    {
+    id: 2,
+    slug: "course-2",
+    title: "Course 2",
+    desc: "A great course 2",
+    cover_image: "http://lorempixel.com/400/200/nature/"
+    },
+    {
+        id: 3,
+        slug: "course-3",
+    title: "Course 3",
+    desc: "A great course 3",
+    cover_image: "http://lorempixel.com/400/200/nature/"
+    }
+];
+
+var homeReducer = (state = featuredState, action) => {
+    console.log(`EXECUTING ${action.type}: `);
+    switch(action.type) {
+        case 'FEATURED_COURSE':
+            return state;
+        default:
+            return state;
+    }
+};
+
+var reducers = combineReducers({
+    home: homeReducer
+});
+
+var store = createStore(reducers, compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+export default store;
